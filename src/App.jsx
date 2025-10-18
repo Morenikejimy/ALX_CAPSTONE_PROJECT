cc3f96121cef4ffc6e4f1eba8cfe5a2f077b9eba
Morenikejimy <amorenikeji198@gmail.com>
2025-10-18 14:02:48 -0700

    update APP>JSX

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import SearchResults from "./pages/SearchResults";
 
 function App() {
   return (
-<<<<<<< HEAD
       <Routes>
          <Route path="/" element={<Home />}/>
           <Route path="/books/:categoryId" element={<BookList />} />
@@ -15,22 +14,6 @@ function App() {
            <Route path="/book-details/:categoryId/:bookId" element={<BookDetails gi/>} />
       </Routes>
       
-=======
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/books/:categoryId" element={<BookList />} />
-      <Route path="/Home/:ageGroupId" element={<BookList />} />
-      <Route path="/search" element={<SearchResults />} />
-      <Route
-        path="/book-details/:categoryId/:bookId"
-        element={<BookDetails />}
-      />
-      <Route
-        path="/read-book/:categoryId/:bookId/:chapterNumber"
-        element={<ReadBook />}
-      />
-    </Routes>
->>>>>>> 8e8ec1660e52843cc5dd124418019e8092ca126e
   );
 }
 
